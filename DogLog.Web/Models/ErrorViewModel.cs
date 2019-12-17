53f568f1e01c2cb56644f5b046775725a4919461
Christopher Plummer <cplummer@reservations.com>
2019-12-17 11:36:00 -0500

    Add project files.

---
+++ DogLog.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DogLog.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
