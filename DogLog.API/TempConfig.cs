6cbb652b40b9c393470a26596aed7dfe452dbba5
Christopher Plummer <cplummer001@gmail.com>
2020-01-06 23:37:33 -0500

    Fixed UI issues Added TempConfig for holding external API keys until proper procedures in place

---
+++ DogLog.API/TempConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogLog.API
+{
+    public class TempConfig
+    {
+        public readonly string DogLogAPIKey = $"c1adc797-933b-4ff0-a67f-2627b369cfdc";
+    }
+}
